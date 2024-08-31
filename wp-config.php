5a40d5fb15e5a61cb843a4779b89402e910d2483
trkhduy <trkhduy003@gmail.com>
2024-08-31 15:16:21 +0700

    Fix: config php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'furniture' );
+define( 'DB_NAME', 'angelieBlanc' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
